f7d89f55878d06fb1b8d249d473fedc27173b086
Neamar <neamar@neamar.fr>
2012-10-16 18:41:26 +0200

    Display context menu

--- src/fr/neamar/summon/record/FavoriteRecord.java
+++ src/fr/neamar/summon/record/FavoriteRecord.java
@@ -1,9 +1,12 @@
 package fr.neamar.summon.record;
 
 import android.content.Context;
+import android.view.ContextMenu;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import fr.neamar.summon.R;
-import fr.neamar.summon.holder.AppHolder;
 import fr.neamar.summon.holder.FavoriteHolder;
 
 public class FavoriteRecord extends Record {
@@ -13,12 +16,38 @@ public class FavoriteRecord extends Record {
 		super();
 		this.holder = this.favoriteHolder = holder;
 	}
+	
+	private View.OnCreateContextMenuListener contextMenu = new View.OnCreateContextMenuListener() {
+
+		@Override
+		public void onCreateContextMenu(ContextMenu arg0, View arg1,
+				ContextMenuInfo arg2) {
+			// TODO Auto-generated method stub
+			arg0.add(0, 0, 0, "Call");
+			arg0.add(0, 1, 0, "Map");
+			arg0.add(0, 2, 0, "Market");
+
+		}
+	};
 
 	@Override
 	public View display(Context context, View v) {
 		if (v == null)
+		{
 			v = inflateFromId(context, R.layout.item_favorites);
 
+			ImageButton favorites = (ImageButton) v.findViewById(R.id.item_favorites_favorites0);
+			favorites.setOnCreateContextMenuListener(contextMenu);
+
+			favorites.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					v.showContextMenu();
+				}
+			});
+		}
+		
 		return v;
 	}
 
